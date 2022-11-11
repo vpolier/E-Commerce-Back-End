e13e2b625bcbc5cc37b9ed45d9acb601a960af95
vpolier <vpolier81@gmail.com>
2022-11-11 22:02:11 +1000

    set up models and api

---
+++ models/index.js
@@ -0,0 +1,40 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  // When we delete a category, make sure to also delete the product.
+  onDelete: 'CASCADE',
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'product_id',
+  });
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id',
+  });
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
